5f687e1d080166c358628b7f3b6ea2b9ac94516f
Big-Cheung <evan.d.cheung@gmail.com>
2024-01-24 10:56:45 +0000

    100% viewport canvas, and package updateds

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,13 @@ import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import frontUrl from "/front.png";
 
 function App() {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    fetch("localhost")
+    console.log(frontUrl);
   }, []);
 
   return (
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    port: 8080,
+  },
   build: {
     outDir: "../public",
-    emptyOutDir: true
-  }
-})
+    emptyOutDir: true,
+  },
+  base: "/",
+});

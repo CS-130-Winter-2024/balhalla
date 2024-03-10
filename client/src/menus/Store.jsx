9ad04f7ab1f46b6c5c8348727ba54595251a8d7e
gpham2 <giangphamgia03@gmail.com>
2024-03-09 23:22:22 -0800

    added the equiping of different types, weapon armor and assessories

--- client/src/menus/Store.jsx
+++ client/src/menus/Store.jsx
@@ -4,7 +4,6 @@ import {
   DialogTitle,
   DialogContent,
   Typography,
-  Slider,
   Grid,
   Card,
   CardMedia,
@@ -13,7 +12,8 @@ import {
   MenuItem,
   FormControl,
   Select,
-  Box
+  Box,
+  Avatar
 } from '@mui/material';
 import PropTypes from 'prop-types';
 
@@ -32,6 +32,8 @@ Store.propTypes = {
       name: PropTypes.string.isRequired,
       image: PropTypes.string.isRequired,
       cost: PropTypes.number.isRequired,
+      type: PropTypes.oneOf(['weapon', 'armor', 'accessory']).isRequired,
+
     })
   ),
   ownedItems: PropTypes.arrayOf(
@@ -40,17 +42,21 @@ Store.propTypes = {
       name: PropTypes.string.isRequired,
       image: PropTypes.string.isRequired,
       cost: PropTypes.number.isRequired,
+      type: PropTypes.oneOf(['weapon', 'armor', 'accessory']).isRequired,
+
     })
   ).isRequired,
   onClose: PropTypes.func.isRequired,
   onBuy: PropTypes.func.isRequired,
-  onEquip: PropTypes.func.isRequired,
-  equippedItem: PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    name: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
-    cost: PropTypes.number.isRequired,
-  }),
+  handleEquip: PropTypes.func.isRequired,
+  equippedItems: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      image: PropTypes.string.isRequired,
+      cost: PropTypes.number.isRequired,
+      type: PropTypes.oneOf(['weapon', 'armor', 'accessory']).isRequired,
+    })).isRequired,
 };
 
 // make copy of object
@@ -58,41 +64,53 @@ function deepCopy(obj) {
     return JSON.parse(JSON.stringify(obj));
 }
 
-const TYPE_MAP = [
-    'weapon',
-    'weapon',
-    'accessory',
-    'armor',
-    'weapon',
-]
+function Store({ isOpen, availableItems, ownedItems, onClose, onBuy, handleEquip, equippedItems }) {
+  const [currWeapon, setCurrWeapon] = useState(deepCopy(equippedItems[0]));
+  const [prevWeapon, setPrevWeapon] = useState(deepCopy(equippedItems[0]));
 
-function Store({ isOpen, availableItems, ownedItems, onClose, onBuy, onEquip, equippedItem }) {
-  const [currentlyEquipped, setCurrentlyEquipped] = useState(deepCopy(equippedItem));
-  const [prevEquippedItem, setPrevEquippedItem] = useState(deepCopy(currentlyEquipped));
-  const ITEMS_COUNT = availableItems.length;
+  const [currArmor, setCurrArmor] = useState(deepCopy(equippedItems[1]));
+    const [prevArmor, setPrevArmor] = useState(deepCopy(equippedItems[1]));
+
+const [currAccessory, setCurrAccessory] = useState(deepCopy(equippedItems[2]));
+const [prevAccessory, setPrevAccessory] = useState(deepCopy(equippedItems[2]));
 
   const handleBuy = (item) => {
     onBuy(item);
   };
 
   const handleEquipChange = (event) => {
+
     const selectedItemId = event.target.value;
     const selectedOwnedItem = ownedItems.find((item) => item.id === selectedItemId);
-    setCurrentlyEquipped(deepCopy(selectedOwnedItem));
-    console.log('Selected item:', selectedOwnedItem)
-    console.log('Currently equipped:', currentlyEquipped)
-    console.log('Prev equipped:', prevEquippedItem)
-  };
-
-  const handleSave = () => {
-    if (currentlyEquipped && (prevEquippedItem === null || currentlyEquipped.id !== prevEquippedItem.id)) {
-      onEquip(deepCopy(currentlyEquipped));
-      setPrevEquippedItem(deepCopy(currentlyEquipped));
+    // finds the type of item
+    const itemType = selectedOwnedItem.type;
+    if (itemType === "weapon") {
+      setCurrWeapon(deepCopy(selectedOwnedItem));
+    }
+    else if (itemType === "armor") {
+        setCurrArmor(deepCopy(selectedOwnedItem));
+    }
+    else if (itemType === "accessory"){
+        setCurrAccessory(deepCopy(selectedOwnedItem));
     }
   };
 
+  const canSave = () => {
+    const armorChanged = currArmor && (prevArmor === null || currArmor.id !== prevArmor.id);
+  const weaponChanged = currWeapon && (prevWeapon === null || currWeapon.id !== prevWeapon.id);
+  const accessoryChanged = currAccessory && (prevAccessory === null || currAccessory.id !== prevAccessory.id);
+    return armorChanged || weaponChanged || accessoryChanged;
+  }
+
+  const handleSave = () => {
+    handleEquip(currWeapon, currArmor, currAccessory);
+    setPrevWeapon(deepCopy(currWeapon));
+    setPrevArmor(deepCopy(currArmor));
+    setPrevAccessory(deepCopy(currAccessory));
+  };
+
   return (
-    <Dialog open={isOpen} onClose={onClose} maxWidth="md" fullWidth style={{ marginLeft: OFFSET }}>
+    <Dialog open={isOpen} onClose={onClose} maxWidth="md" fullWidth style={{ marginLeft: OFFSET}}>
       <DialogTitle style={{ fontFamily, color: primaryColor }}>Shop</DialogTitle>
       <DialogContent>
         <Grid container spacing={2}>
@@ -111,33 +129,33 @@ function Store({ isOpen, availableItems, ownedItems, onClose, onBuy, onEquip, eq
                       alt={item.name}
                       style={ownedItems.some((ownedItem) => ownedItem.id === item.id) ? { filter: 'grayscale(100%)' } : {}}
                     />
-                    <CardContent style={{height: "100%"}}>
-                        <Box style={{width: "100%", height: "80%", display: "flex", flexDirection: "column", justifyContent: "flex-start", alignItems: 'center'}}>
+                    <CardContent style={{ height: "100%" }}>
+                      <Box style={{ width: "100%", height: "80%", display: "flex", flexDirection: "column", justifyContent: "flex-start", alignItems: 'center' }}>
                         <Typography variant="subtitle1">{item.name}</Typography>
-                        <Typography variant="caption" color={primaryColor}> {TYPE_MAP[item.id % ITEMS_COUNT]} </Typography>
-                      {ownedItems.some((ownedItem) => ownedItem.id === item.id) ? (
-                        <Typography variant="caption" color="textSecondary">
-                          OWNED
-                        </Typography>
-                      ) : (
-                        <Box style={{display: "flex", height: "100%", width: "100%", alignItems: "center", justifyContent: "space-around", marginBottom: 20}}> 
-                        <Button
-                          variant="contained"
-                          color="primary"
-                          disabled={ownedItems.some((ownedItem) => ownedItem.id === item.id)}
-                          onClick={() => handleBuy(item)}
-                          style={{width: "30%", maxHeight: "20%"}}
-                        >
-                          Buy
-                        </Button>
-                        <Typography variant="subtitle2"  style={{fontFamily, fontWeight: "bold", color: "#C8b273"}}>
-                            {item.cost} C
-                        </Typography>
-                        </Box>
-                      )}
+                        <Typography variant="caption" color={primaryColor}> {item.type} </Typography>
+                        {ownedItems.some((ownedItem) => ownedItem.id === item.id) ? (
+                          <Typography variant="caption" color="textSecondary">
+                            OWNED
+                          </Typography>
+                        ) : (
+                            <Box style={{ display: "flex", height: "100%", width: "100%", alignItems: "center", justifyContent: "space-around", marginBottom: 20 }}>
+                              <Button
+                                variant="contained"
+                                color="primary"
+                                disabled={ownedItems.some((ownedItem) => ownedItem.id === item.id)}
+                                onClick={() => handleBuy(item)}
+                                style={{ width: "30%", maxHeight: "20%" }}
+                              >
+                                Buy
+                              </Button>
+                              <Typography variant="subtitle2" style={{ fontFamily, fontWeight: "bold", color: "#C8b273" }}>
+                                {item.cost} C
+                              </Typography>
+                            </Box>
+                          )}
 
-                        </Box>
-                      
+                      </Box>
+
                     </CardContent>
                   </Card>
                 </Grid>
@@ -148,25 +166,99 @@ function Store({ isOpen, availableItems, ownedItems, onClose, onBuy, onEquip, eq
             {/* Right Side: Equip section */}
             <FormControl fullWidth>
               <Typography variant="h6" gutterBottom style={{ fontFamily, color: primaryColor }}>
-                Equip Item
+                Equip Weapon
               </Typography>
-              <Select value={currentlyEquipped ? currentlyEquipped.id : ''} onChange={handleEquipChange}>
-                {ownedItems.map((item) => (
+
+              {/* Select for Weapon */}
+              <Select value={currWeapon ? currWeapon.id : ''} onChange={handleEquipChange} style={{height: 40, marginBottom: 10}}>
+
+                {ownedItems.filter((e)=> e.type === 'weapon').map((item) => (
                   <MenuItem key={item.id} value={item.id}>
                     {item.name}
                   </MenuItem>
                 ))}
               </Select>
+              <Typography variant="h6" gutterBottom style={{ fontFamily, color: primaryColor }}>
+                Equip Armor
+              </Typography>
+
+              {/* Select for Armor */}
+              <Select value={currArmor ? currArmor.id : ''} onChange={handleEquipChange}  style={{height: 40, marginBottom: 10}}>
+
+                {ownedItems.filter((e)=> e.type === 'armor').map((item) => (
+                  <MenuItem key={item.id} value={item.id}>
+                    {item.name}
+                  </MenuItem>
+                ))}
+              </Select>
+              <Typography variant="h6" gutterBottom style={{ fontFamily, color: primaryColor }}>
+                Equip Assessory
+              </Typography>
+
+              {/* Select for Assessories */}
+              <Select value={currAccessory ? currAccessory.id : ''} onChange={handleEquipChange}  style={{height: 40, marginBottom: 10}}>
+
+                {ownedItems.filter((e)=> e.type === 'accessory').map((item) => (
+                  <MenuItem key={item.id} value={item.id}>
+                    {item.name}
+                  </MenuItem>
+                ))}
+              </Select>
+              
+              
               <Button
                 variant="contained"
                 color="primary"
                 onClick={handleSave}
-                disabled={!(currentlyEquipped && (prevEquippedItem === null || currentlyEquipped.id !== prevEquippedItem.id))}
+                // disabled={!(currentlyEquipped && (prevEquippedItem === null || currentlyEquipped.id !== prevEquippedItem.id))}
+                disabled={!canSave()}
                 style={{ marginTop: '16px' }}
               >
                 Save
               </Button>
-              
+              {equippedItems[0] && (
+                <Box style={{ marginTop: '16px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                  <Typography variant="subtitle1" style={{ marginBottom: '8px' }}>
+                    Equipped Weapon:
+                  </Typography>
+                  <Avatar style={{ border: `2px solid ${primaryColor}`, width: '80px', height: '80px' }}>
+                    <img
+                      src={equippedItems[0].image}
+                      alt={equippedItems[0].name}
+                      style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                    />
+                  </Avatar>
+                </Box>
+              )}
+              {equippedItems[1] && (
+                <Box style={{ marginTop: '16px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                  <Typography variant="subtitle1" style={{ marginBottom: '8px' }}>
+                    Equipped Armor:
+                  </Typography>
+                  <Avatar style={{ border: `2px solid ${primaryColor}`, width: '80px', height: '80px' }}>
+                    <img
+                      src={equippedItems[1].image}
+                      alt={equippedItems[1].name}
+                      style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                    />
+                  </Avatar>
+                </Box>
+              )}
+              {equippedItems[2] && (
+                <Box style={{ marginTop: '16px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                  <Typography variant="subtitle1" style={{ marginBottom: '8px' }}>
+                    Equipped Accessory:
+                  </Typography>
+                  <Avatar style={{ border: `2px solid ${primaryColor}`, width: '80px', height: '80px' }}>
+                    <img
+                      src={equippedItems[2].image}
+                      alt={equippedItems[2].name}
+                      style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+                    />
+                  </Avatar>
+                </Box>
+              )}
+
             </FormControl>
           </Grid>
         </Grid>
--- client/src/ui/UI.jsx
+++ client/src/ui/UI.jsx
@@ -48,7 +48,10 @@ function DisplayComponent() {
   );
 }
 
-export default function UI({}) {
+
+
+
+export default function UI({showAlert}) {
   const [showOverlay, setShowOverlay] = useState(false);
   const [username, setUsername] = useState("Hello World");
   const [loginClicked, setLoginClicked] = useState(false);
@@ -59,18 +62,20 @@ export default function UI({}) {
     return JSON.parse(JSON.stringify(obj));
   }
 
+
+
   const [showStore, setShowStore] = useState(true);
   const ITEMS_MAP = [
-    { id: 1, cost: 200, name: 'Lich King Sword', image: 'https://cdn.discordapp.com/attachments/371115539365494794/1216243141296914432/il_570xN.png?ex=65ffadb1&is=65ed38b1&hm=1f39c8935a44029669b98b238476b0b0527b3b38ded791244a73b2ef0b9c4c64&' },
-    { id: 2, cost: 30, name: 'Link Kit', image: 'https://cdn.discordapp.com/attachments/371115539365494794/1216243316409241750/1_51e42ba9-bb2a-4d76-bd31-2e3e9125b043_1200x.png?ex=65ffaddb&is=65ed38db&hm=f0d74a63a49f6c79f495143cd90e4e98552d43b7fcf1c288f1a11c6ae057f092&' },
-    { id: 3, cost: 150, name: 'Omnitrix', image: 'https://cdn.discordapp.com/attachments/371115539365494794/1216243518641672302/ben-10-omnitrix-3d-model-83155920b5.png?ex=65ffae0b&is=65ed390b&hm=9aefefd4c18fb88da3c60fe16e71a9358d73ef116660540baa95945f6a013e57&' },
-    { id: 4, cost: 25, name: 'Levi Gear', image: 'https://cdn.discordapp.com/attachments/371115539365494794/1216243824943304725/DSC05199-12.png?ex=65ffae54&is=65ed3954&hm=be899a93ee994773b52e7ffa85e356309c61b8c40d803ef498d534a2a4b4c2cf&'},
-    {id: 5, cost: 35, name: 'Kaiba Duel Disk', image: 'https://cdn.discordapp.com/attachments/371115539365494794/1216243947253399612/Battle20City20Duel20Disk201.png?ex=65ffae71&is=65ed3971&hm=b9a22ef0763703a0f02fc0f891e89ae3e261fd846a9e3fe9e314d8b84e0c1da9&'}
+    { id: 1, type: 'weapon', cost: 200, name: 'Frostmourne', image: 'https://cdn.discordapp.com/attachments/371115539365494794/1216243141296914432/il_570xN.png?ex=65ffadb1&is=65ed38b1&hm=1f39c8935a44029669b98b238476b0b0527b3b38ded791244a73b2ef0b9c4c64&' },
+    { id: 2, type: 'weapon', cost: 30, name: 'Master Sword', image: 'https://cdn.discordapp.com/attachments/371115539365494794/1216243316409241750/1_51e42ba9-bb2a-4d76-bd31-2e3e9125b043_1200x.png?ex=65ffaddb&is=65ed38db&hm=f0d74a63a49f6c79f495143cd90e4e98552d43b7fcf1c288f1a11c6ae057f092&' },
+    { id: 3, type: 'accessory', cost: 150, name: 'Omnitrix', image: 'https://cdn.discordapp.com/attachments/371115539365494794/1216243518641672302/ben-10-omnitrix-3d-model-83155920b5.png?ex=65ffae0b&is=65ed390b&hm=9aefefd4c18fb88da3c60fe16e71a9358d73ef116660540baa95945f6a013e57&' },
+    { id: 4, type: 'armor', cost: 25, name: 'Levi Gear', image: 'https://cdn.discordapp.com/attachments/371115539365494794/1216243824943304725/DSC05199-12.png?ex=65ffae54&is=65ed3954&hm=be899a93ee994773b52e7ffa85e356309c61b8c40d803ef498d534a2a4b4c2cf&'},
+    {id: 5, type: 'accessory', cost: 35, name: 'Kaiba Duel Disk', image: 'https://cdn.discordapp.com/attachments/371115539365494794/1216243947253399612/Battle20City20Duel20Disk201.png?ex=65ffae71&is=65ed3971&hm=b9a22ef0763703a0f02fc0f891e89ae3e261fd846a9e3fe9e314d8b84e0c1da9&'}
   ];
 
   // array of item objects
   const [allItems, setAllItems] = useState(deepCopy(ITEMS_MAP));
-  const [equipItem, setEquipItem] = useState(null);
+  const [equippedItems, setEquippedItems] = useState([null, null, null]); // 0 is weapon, 1 is armor, 2 is accessory
   const [ownedItems, setOwnedItems] = useState([]);
 
 
@@ -78,9 +83,7 @@ export default function UI({}) {
   useEffect(() => {
     setAllItems(deepCopy(ITEMS_MAP));
     setOwnedItems([]);
-    console.log('allItems:', allItems);
-    console.log('ownedItems:', ownedItems);
-    console.log('equipItem:', equipItem)
+    setEquippedItems([null, null, null]);
   }, []);
 
 
@@ -101,9 +104,8 @@ export default function UI({}) {
     setShowStore(false);
   };
 
-  const handleEquip = (item) => {
-    console.log(`Equipping ${item.name} with id ${item.id}`);
-    setEquipItem(item);
+  const handleEquip = (weapon, armor, accessory) => {
+    setEquippedItems(deepCopy([weapon, armor, accessory]));
   }
   // END Giang's stuff
 
@@ -127,8 +129,9 @@ export default function UI({}) {
             ownedItems={ownedItems} 
             onClose={handleClose}
             onBuy={handleBuy}
-            onEquip={handleEquip}
-            equippedItem={equipItem}
+            equippedItems={equippedItems}
+            handleEquip={handleEquip}
+            showAlert={showAlert}
       />
       {/* END: Giang's stuff */}
       <div id="UI" style={{ display: (showOverlay && "none") || "block" }}>

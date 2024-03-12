e64cfee93b40ca25c2ebd9021e68bae94c5f00b2
Vinh Nguyen <vinhsnguyen86@gmail.com>
2024-03-11 19:35:38 -0700

    more documentation for OtherPlayers.jsx, Game.jsx, and Player.jsx

--- client/src/game/Player.jsx
+++ client/src/game/Player.jsx
@@ -8,6 +8,7 @@ var spectateCamera;
 
 var spectating = false
 
+// Properties of the client's player in-game
 var properties = {
   x: 0,
   y: constants.ALIVE_Y,
@@ -15,6 +16,7 @@ var properties = {
   directionHeld: [0, 0, 0, 0],
   hasBall: false,
 };
+// Meta data of the client player (player's points, selected ball, selected pet, etc.)
 var myMetadata;
 
 var locked = false; //Locked = First Person Cam; Unlocked = Mouse Movement
@@ -24,9 +26,10 @@ var perpVector = new three.Vector3();
 var intermediateVector = new three.Vector3();
 var prevCamVector = new three.Vector3();
 
-var dashScalar = 1;
-var dashAvailable = true;
+var dashScalar = 1; // always applied to movements, greater than 1 when dash is used
+var dashAvailable = true; // based on dash cooldown, defines when dash can be used
 
+// Uses camera direction and movement key presses to determine player's movement direction
 function calculateDirection() {
   camera.getWorldDirection(prevCamVector);
   camera.getWorldDirection(movementVector);
@@ -41,22 +44,23 @@ function calculateDirection() {
     0,
     perpVector.x * (properties.directionHeld[3] - properties.directionHeld[1]),
   );
-  movementVector.add(perpVector);
-  movementVector.y = 0;
-  movementVector.normalize();
+  movementVector.add(perpVector); // combining horizontal and vertical movements along arena
+  movementVector.y = 0; // regardless of camera, player an only move along x and z axes
+  movementVector.normalize(); // Must normalize movements because all players move at same speed
 }
 
-var canSend = true;
+var canSend = true; // active when a message can be sent to server to avoid msg flooding, decided by UPDATE_RATE
 function sendMovement(override = false) {
-  if (spectating) return;
-  if (canSend || override) {
+  if (spectating) return; // spectating if in lobby or dead, so can't move in that case
+  if (canSend || override) { // override allows for dashing and unlocking to get past the UPDATE_RATE limit
     getSocket().send(
       JSON.stringify([
         constants.MESSAGES.sendMovement,
-        { direction: [movementVector.x * dashScalar, movementVector.z * dashScalar] },
+        { direction: [movementVector.x * dashScalar, movementVector.z * dashScalar] }, // always applies dashScalar, it is usually just 1
       ]),
     );
     canSend = false;
+    // UPDATE_RATE decides how often we can send updates to server
     setTimeout(() => {
       canSend = true;
     }, constants.UPDATE_RATE);
@@ -64,7 +68,8 @@ function sendMovement(override = false) {
 }
 
 function throwBall() {
-  if (locked && properties.hasBall) {
+  if (locked && properties.hasBall) { // can only throw ball if not in menu and if you are holding ball
+    // sends camera direction to server to handle ball velocity
     camera.getWorldDirection(intermediateVector);
     getSocket().send(
       JSON.stringify([
@@ -81,18 +86,19 @@ function onKeyDown(e) {
   if (locked) {
     let wasMovement = false;
 
-    if (e.key in constants.MOVEMENT_MAP) {
+    if (e.key in constants.MOVEMENT_MAP) { // Movement keys: WASD
       let index = constants.MOVEMENT_MAP[e.key];
       wasMovement = properties.directionHeld[index] == 0;
       properties.directionHeld[index] = 1;
     }
 
-    if (e.key == "Shift"){
+    if (e.key == "Shift"){ // dash should only trigger on key press, not release
       if (dashAvailable){
         wasMovement = true;
         dashScalar = constants.DASH_SPEED;
-        // will jump the player in their moving direction
+        // will fast forward the player in their moving direction
   
+        // starting dash cooldown
         dashAvailable = false;
         setTimeout(() => {
           dashAvailable = true;
@@ -105,6 +111,9 @@ function onKeyDown(e) {
       
     }
 
+    // Only calculating and sending movements to server when wasMovement is true.
+    // wasMovement is true when the the movement keys are being pressed differently than before.
+    // Avoids wasting time recalculating and resending the same movement vector over and over again.
     if (wasMovement) {
       calculateDirection();
       sendMovement(dashScalar==constants.DASH_SPEED);
@@ -118,7 +127,7 @@ function onKeyUp(e) {
   if (locked) {
     let wasMovement = false;
 
-    if (e.key in constants.MOVEMENT_MAP) {
+    if (e.key in constants.MOVEMENT_MAP) { // Movement keys: WASD
       let index = constants.MOVEMENT_MAP[e.key];
       wasMovement = true;
       properties.directionHeld[index] = 0;
@@ -126,11 +135,15 @@ function onKeyUp(e) {
       throwBall();
     }
 
+    // resets dashScalar after dash is used
     if (dashScalar > 1){
       wasMovement = true;
       dashScalar = 1;
     }
     
+    // Only calculating and sending movements to server when wasMovement is true.
+    // wasMovement is true when the the movement keys are being pressed differently than before.
+    // Avoids wasting time recalculating and resending the same movement vector over and over again.
     if (wasMovement) {
       calculateDirection();
       sendMovement();
@@ -154,6 +167,7 @@ export function attachKeybinds() {
   });
 }
 
+// default spectating camera for when player is dead or in lobby
 function createSpectateCamera() {
   spectateCamera = new three.PerspectiveCamera(75, 2, 0.1, 1000);
   spectateCamera.position.z = 0;
@@ -164,6 +178,7 @@ function createSpectateCamera() {
   spectateCamera.updateProjectionMatrix();
 }
 
+// player in-game camera for when game is active and player is alive
 export function createCamera(spectate = false) {
   camera = new three.PerspectiveCamera(75, 2, 0.1, 1000);
   camera.position.z = properties.z;

4de7993b7757130294a6272bfe6ba55134232279
Evan Cheung <evan.d.cheung@gmail.com>
2024-03-12 21:20:20 -0700

    Added some icons, fixed giang menu, rebase patch

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import game from './game/Game.jsx'
 import InGameMenu from './ui/components/InGameMenu.jsx'
 import AlertWrapper from './ui/components/AlertWrapper.jsx'
 import LogWrapper from './ui/components/LogWrapper.jsx'
-import EndScreen from './menus/EndScreen.jsx'
+import EndScreen from './ui/components/EndScreen.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -17,10 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
         {showAlert => (
           <>
-            <EndScreen
-              open={true}
-              onClose={() => {}}
-              />
+            <EndScreen/>
             <UI showAlert={showAlert}/>
 
             <InGameMenu
